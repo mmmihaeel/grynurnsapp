898c6a547e11d93ea67f212eaa3ed55743b98370
mmmihhaeel <mikael11032005@gmail.com>
2024-05-19 16:25:41 +0300

    test deployment

--- src/actions/get-products.ts
+++ src/actions/get-products.ts
@@ -1,20 +1,24 @@
 'use server';
-
-import { host } from '@/i18n.config';
 import { Product } from '@/types/product.type';
+import fs from 'fs/promises';
+import path from 'path';
 
 const getProducts = async () => {
-	const url = `${host}/products/products.json`;
-	console.log(url);
-	const res = await fetch(url, {
-		cache: 'force-cache',
-	});
-	if (!res.ok) {
-		throw new Error(
-			`Failed to fetch data! Status code: ${res.status}, error message: ${res.text}`
-		);
-	}
-	return res.json() as unknown as Product[];
+	// const url = `${host}/products/products.json`;
+	// console.log(url);
+	// const res = await fetch(url, {
+	// 	cache: 'force-cache',
+	// });
+	const data = await fs.readFile(
+		path.join(process.cwd(), '/public/products/products.json')
+	);
+	const json = JSON.parse(data.toString());
+	// if (!res.ok) {
+	// 	throw new Error(
+	// 		`Failed to fetch data! Status code: ${res.status}, error message: ${res.text}`
+	// 	);
+	// }
+	return json as unknown as Product[];
 };
 
 export default getProducts;
