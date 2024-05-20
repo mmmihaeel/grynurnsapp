d31bf576f1f9f420e4c3c1600db4459c0fc43418
mmmihhaeel <mikael11032005@gmail.com>
2024-05-20 23:30:22 +0300

    fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,38 +6,43 @@ export default createMiddleware({
 	pathnames,
 	localePrefix,
 	defaultLocale: defaultLocale,
-	// domains: [
-	// 	{
-	// 		domain: 'grynurns.com',
-	// 		defaultLocale: 'en-GB',
-	// 		locales: ['en-GB'],
-	// 	},
-	// 	{
-	// 		domain: 'grynurns.fr',
-	// 		defaultLocale: 'fr-FR',
-	// 		locales: ['fr-FR'],
-	// 	},
-	// 	{
-	// 		domain: 'grynurns.pl',
-	// 		defaultLocale: 'pl-PL',
-	// 		locales: ['pl-PL'],
-	// 	},
-	// 	{
-	// 		domain: 'grynurns.de',
-	// 		defaultLocale: 'de-DE',
-	// 		locales: ['de-DE'],
-	// 	},
-	// 	{
-	// 		domain: 'grynurns.uk',
-	// 		defaultLocale: 'en-GB',
-	// 		locales: ['en-GB'],
-	// 	},
-	// 	{
-	// 		domain: 'grynurns.com.ua',
-	// 		defaultLocale: 'uk-UA',
-	// 		locales: ['uk-UA'],
-	// 	},
-	// ],
+	domains: [
+		{
+			domain: String(process.env.VERCEL_URL),
+			defaultLocale: 'en-GB',
+			locales: ['en-GB', 'de-DE', 'fr-FR', 'pl-PL', 'uk-UA'],
+		},
+		{
+			domain: String(process.env.COM_DOMAIN),
+			defaultLocale: 'en-GB',
+			locales: ['en-GB', 'de-DE', 'fr-FR', 'pl-PL', 'uk-UA'],
+		},
+		{
+			domain: String(process.env.FR_DOMAIN),
+			defaultLocale: 'fr-FR',
+			locales: ['fr-FR'],
+		},
+		{
+			domain: String(process.env.PL_DOMAIN),
+			defaultLocale: 'pl-PL',
+			locales: ['pl-PL'],
+		},
+		{
+			domain: String(process.env.DE_DOMAIN),
+			defaultLocale: 'de-DE',
+			locales: ['de-DE'],
+		},
+		{
+			domain: String(process.env.EN_DOMAIN),
+			defaultLocale: 'en-GB',
+			locales: ['en-GB'],
+		},
+		{
+			domain: String(process.env.UK_DOMAIN),
+			defaultLocale: 'uk-UA',
+			locales: ['uk-UA'],
+		},
+	],
 });
 
 export const config = {
