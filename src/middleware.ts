02b90cdf92c2926ec7523aa9b05e7ae3617d58a2
mmmihhaeel <mikael11032005@gmail.com>
2024-05-21 21:53:08 +0300

    changed photo

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,43 +7,43 @@ export default createMiddleware({
 	localePrefix,
 	defaultLocale: defaultLocale,
 	alternateLinks: true,
-	// domains: [
-	// 	{
-	// 		domain: String(process.env.VERCEL_URL),
-	// 		defaultLocale: 'en-GB',
-	// 		locales: ['en-GB', 'de-DE', 'fr-FR', 'pl-PL', 'uk-UA'],
-	// 	},
-	// 	{
-	// 		domain: String(process.env.COM_DOMAIN),
-	// 		defaultLocale: 'en-GB',
-	// 		locales: ['en-GB', 'de-DE', 'fr-FR', 'pl-PL', 'uk-UA'],
-	// 	},
-	// 	{
-	// 		domain: String(process.env.FR_DOMAIN),
-	// 		defaultLocale: 'fr-FR',
-	// 		locales: ['fr-FR'],
-	// 	},
-	// 	{
-	// 		domain: String(process.env.PL_DOMAIN),
-	// 		defaultLocale: 'pl-PL',
-	// 		locales: ['pl-PL'],
-	// 	},
-	// 	{
-	// 		domain: String(process.env.DE_DOMAIN),
-	// 		defaultLocale: 'de-DE',
-	// 		locales: ['de-DE'],
-	// 	},
-	// 	{
-	// 		domain: String(process.env.EN_DOMAIN),
-	// 		defaultLocale: 'en-GB',
-	// 		locales: ['en-GB'],
-	// 	},
-	// 	{
-	// 		domain: String(process.env.UK_DOMAIN),
-	// 		defaultLocale: 'uk-UA',
-	// 		locales: ['uk-UA'],
-	// 	},
-	// ],
+	domains: [
+		{
+			domain: String(process.env.VERCEL_URL),
+			defaultLocale: 'en-GB',
+			locales: ['en-GB', 'de-DE', 'fr-FR', 'pl-PL', 'uk-UA'],
+		},
+		{
+			domain: String(process.env.COM_DOMAIN),
+			defaultLocale: 'en-GB',
+			locales: ['en-GB', 'de-DE', 'fr-FR', 'pl-PL', 'uk-UA'],
+		},
+		{
+			domain: String(process.env.FR_DOMAIN),
+			defaultLocale: 'fr-FR',
+			locales: ['fr-FR'],
+		},
+		{
+			domain: String(process.env.PL_DOMAIN),
+			defaultLocale: 'pl-PL',
+			locales: ['pl-PL'],
+		},
+		{
+			domain: String(process.env.DE_DOMAIN),
+			defaultLocale: 'de-DE',
+			locales: ['de-DE'],
+		},
+		{
+			domain: String(process.env.EN_DOMAIN),
+			defaultLocale: 'en-GB',
+			locales: ['en-GB'],
+		},
+		{
+			domain: String(process.env.UK_DOMAIN),
+			defaultLocale: 'uk-UA',
+			locales: ['uk-UA'],
+		},
+	],
 });
 
 export const config = {
